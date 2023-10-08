b12f669c8059f396ad7e92ef0345bd4be008317a
ayoubqrt <ayoub.mstk@gmail.com>
2023-10-08 17:32:04 +0200

    feat: delete old react front template

--- src/main/js/src/_mock/account.js
+++
@@ -1,9 +0,0 @@
-// ----------------------------------------------------------------------
-
-const account = {
-  displayName: 'Olivier RICHARD',
-  email: 'olivier.richard@utbm.fr',
-  photoURL: '/assets/images/avatars/avatar_default.jpg',
-};
-
-export default account;
