8e14ffd697ec21260b16e7f8bd91aee6993635a1
Lilian BOUVIER <lilian.bouvier@etu.unistra.fr>
2023-10-25 15:00:05 +0200

    WIP: basic forms

--- src/main/js/src/pages/index.tsx
+++ src/main/js/src/pages/index.tsx
@@ -1,5 +1,38 @@
 export default function Home() {
   return (
-    <div className="btn btn-primary">Home sweet home</div>
+    <div
+      data-theme='light'
+      className='w-screen h-screen p-2 lg:p-6 overflow-y-auto scrollbar-hide scroll-smooth'
+    >
+      <main className='flex flex-col justify-center items-center'>
+        {/* Question */}
+        <section className='hero min-h-screen p-1 pb-12' >
+          <div className='hero-content text-center w-full max-w-lg'>
+            <form className='text-left flex flex-col w-full' method='POST' action='javascript:void(0)'>
+              <h1 className='text-lg font-bold'>
+                This is a statement. Be sure your input here is greatly valued :)
+              </h1>
+              <p className='pt-2 pb-4 text-sm'>
+                You'll find a special gift in your inbox as soon as completed!
+              </p>
+              <button
+                className='btn btn-primary self-end relative'
+                onClick={() => {}}
+              >
+               Done
+              <span className='pt-1 pl-1'>
+                <img 
+                  src='/icons/enter.svg'
+                  height={11}
+                  width={11}
+                />
+                </span>
+              </button>
+            </form>
+          </div>
+        </section>
+      </main>
+
+    </div>
   );
 }
