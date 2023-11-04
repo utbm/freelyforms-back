f5e4c4201277111002828b5e84a9440a8a9a8aa2
ayoubqrt <ayoub.mstk@gmail.com>
2023-11-04 10:33:59 +0100

    feat(front): added typed client of java api

--- src/main/js/src/main.tsx
+++ src/main/js/src/main.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createApiClient } from "./apiClient/client";
 
 import "./index.css";
-import Home from "./pages/index";
+import Home from "./pages/Home";
 import FourOFour from "./pages/404";
+
+export const apiclient = createApiClient(
+	(method, url, params) =>
+		fetch(url, {
+			method,
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(params),
+		}).then((res) => res.json()),
+	"http://localhost:8080",
+);
 
 const router = createBrowserRouter([
 	{
