3472b4f2b49f4dcbaed792c872a5f9751b8fc145
Matthieu Delisle <mdelisle1@hotmail.com>
2023-11-14 13:51:42 +0100

    added material objects

--- src/main/js/src/pages/index.tsx
+++ src/main/js/src/pages/index.tsx
@@ -1,19 +1,51 @@
+import React from 'react';
+import { MapContainer, TileLayer } from 'react-leaflet';
+import Material from './Material';
+import './leaflet.css';
+import './styles.css';
 
-import { MapContainer } from 'react-leaflet/MapContainer';
-import { TileLayer } from 'react-leaflet/TileLayer';
-import "./leaflet.css"
-import "./styles.css"
-export default function Home() {
-  return (
-    <MapContainer
-      center={[51.505, -0.09]}
-      zoom={13}
-      style={{ height: "900px", width: "100%" }}
-    >
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      />
-    </MapContainer>
-  );
+type MaterialType = {
+  id: string;
+  type: string;
+  location: string;
+};
+
+type AppState = {
+  materials: MaterialType[];
+};
+
+class Home extends React.Component<{}, AppState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      materials: [
+        { id: '1', type: 'Type 1', location: 'Location 1' },
+        { id: '2', type: 'Type 2', location: 'Location 2' },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <div style={{ display: 'flex' }}>
+        <MapContainer
+          center={[51.505, -0.09]}
+          zoom={13}
+          style={{ height: '900px', width: '50%' }}
+        >
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
+        </MapContainer>
+        <div style={{ width: '50%' }}>
+          {this.state.materials.map((material) => (
+            <Material key={material.id} material={material} />
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
+
+export default Home;
