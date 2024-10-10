af2b70cc33a6fb1fb9a0d21ae7239949a93fcbde
Agonis Sh <agonis-shala@hotmail.fr>
2024-10-10 14:02:57 +0200

    refactor(app) added info to app.properties to launch it from local if wanted

--- src/main/java/com/utbm/da50/freelyform/configuration/JwtTokenService.java
+++ src/main/java/com/utbm/da50/freelyform/configuration/JwtTokenService.java
@@ -6,6 +6,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -18,8 +19,9 @@ import java.util.function.Function;
 @Service
 public class JwtTokenService {
 
-    // Get secret key from .env file
-    private static final String SECRET_KEY = System.getenv("JWT_SECRET");
+    // Get secret key from properties file
+    @Value("${jwt_secret}")
+    private String SECRET_KEY;
     private static final int TOKEN_DURATION = 1000*60*60*48; // 48 hours
 
 
