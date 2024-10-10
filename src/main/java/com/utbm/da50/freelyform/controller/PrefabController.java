3c2a55c4e62f2f3aa385aa4d20b224bb101e3607
okarim02 <ounis.karim008@gmail.com>
2024-10-10 17:46:26 +0200

    refactor(crud-prefab): enhance code 2. Add createdAt and updatedAt Enhance code

--- src/main/java/com/utbm/da50/freelyform/controller/PrefabController.java
+++ src/main/java/com/utbm/da50/freelyform/controller/PrefabController.java
@@ -4,6 +4,10 @@ import com.utbm.da50.freelyform.dto.PrefabInput;
 import com.utbm.da50.freelyform.dto.PrefabOutput;
 import com.utbm.da50.freelyform.model.Prefab;
 import com.utbm.da50.freelyform.service.PrefabService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,14 +15,25 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
+@Tag(name = "Prefab API", description = "Endpoints for managing prefabs")
 @RequestMapping("/v1/prefabs")
 @RestController
 public class PrefabController {
-    @Autowired
-    private PrefabService service;
 
+    private final PrefabService service;
+
+    @Autowired
+    public PrefabController( PrefabService service) {
+        this.service = service;
+    }
+
+    @Operation(summary = "Get a list of all prefabs")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "List of prefabs returned")
+    })
     @GetMapping("")
     @ResponseBody
     public ResponseEntity<List<PrefabOutput>> getAllPrefabs() {
@@ -26,41 +41,64 @@ public class PrefabController {
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get a prefab by its unique id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Prefab returned"),
+            @ApiResponse(responseCode = "400", description = "Bad request"),
+            @ApiResponse(responseCode = "404", description = "Prefab not found"),
+    })
     @ResponseBody
-    public ResponseEntity<PrefabOutput> getPrefab(@PathVariable String id) {
+    public ResponseEntity<PrefabOutput> getPrefabById(@PathVariable String id) {
         try {
-            return ResponseEntity.ok(service.getPrefab(id).toRest());
-        } catch (Exception exception) {
+            return ResponseEntity.ok(service.getPrefabById(id).toRest());
+        } catch (NoSuchElementException exception) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, exception.getMessage());
         }
     }
 
     @PostMapping("")
+    @Operation(summary = "Create a new prefab")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Prefab created"),
+            @ApiResponse(responseCode = "400", description = "Bad request")
+    })
     @ResponseBody
-    public ResponseEntity<PrefabInput> createPrefab(@RequestBody PrefabInput newPrefab){
+    public ResponseEntity<PrefabOutput> createPrefab(@RequestBody PrefabInput newPrefab){
         try {
             return ResponseEntity.ok(service.createPrefab(newPrefab.toPrefab()).toRest());
-        } catch (Exception exception) {
+        } catch (NoSuchElementException exception) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, exception.getMessage());
         }
     }
 
+    @Operation(summary = "Update an existing prefab")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Prefab updated"),
+            @ApiResponse(responseCode = "400", description = "Bad request"),
+            @ApiResponse(responseCode = "404", description = "Prefab not found")
+    })
     @PutMapping("/{id}")
     @ResponseBody
-    public ResponseEntity<PrefabInput> updatePrefab(@PathVariable String id, @RequestBody PrefabInput pref) {
+    public ResponseEntity<PrefabOutput> updatePrefab(@PathVariable String id, @RequestBody PrefabInput pref) {
         try {
             return ResponseEntity.ok(service.updatePrefab(id, pref.toPrefab()).toRest());
-        } catch (Exception exception) {
+        } catch (NoSuchElementException exception) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, exception.getMessage());
         }
     }
 
+    @Operation(summary = "Delete an existing prefab")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Prefab deleted"),
+            @ApiResponse(responseCode = "400", description = "Bad request"),
+            @ApiResponse(responseCode = "404", description = "Prefab not found")
+    })
     @DeleteMapping("/{id}")
     @ResponseBody
     public ResponseEntity<PrefabOutput> deletePrefab(@PathVariable String id) {
         try {
             return ResponseEntity.ok(service.deletePrefab(id).toRest());
-        } catch (Exception exception) {
+        } catch (NoSuchElementException exception) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, exception.getMessage());
         }
     }
--- src/main/java/com/utbm/da50/freelyform/dto/PrefabOutput.java
+++ src/main/java/com/utbm/da50/freelyform/dto/PrefabOutput.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @EqualsAndHashCode(callSuper = true)
@@ -12,9 +13,16 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class PrefabOutput extends PrefabInput{
-    String id;
-    public PrefabOutput(String id, String name, String description, String[] tags, List<GroupInput> groups) {
+    private String id;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public PrefabOutput(String id, String name, String description,
+                        LocalDateTime createdAt, LocalDateTime updatedAt,
+                        String[] tags, List<GroupInput> groups) {
         super(name, description, tags, groups);
         this.id = id;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
     }
 }
